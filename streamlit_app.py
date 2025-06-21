d426d727e0333fbfb1ea840b59979de35d11a92d
mushfique2710 <mushfique.pavel@brickeye.com>
2025-06-21 15:22:16 +0000

    Before chatgpt

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,34 @@
 import streamlit as st
 import pandas as pd
+import datetime
 #import matplotlib.pyplot as plt
-st.title("💸 Amex Spending Chart")
+st.title("💸 Credit Card Spending Chart")
 st.write("Get insights on your spending")
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
+    # convert the 'date' column to datetime
+    df['Date'] = pd.to_datetime(df['Date'], format = "%d/%m/%Y")
     st.subheader("Data Preview")
     st.write(df.head())
 
     st.subheader("Filter Data")
-    columns = df.columns.tolist()
-    selected_columns = st.multiselect("Select columns to filter by", columns)
-    filtered_df = df[selected_columns]
-    st.subheader("Summary of the selected category")
+    columns = df[['Date', 'Sub-Category', 'Charges $']]
+    filtered_df = columns
     st.write(filtered_df.head())
-    
-    #st.subheader("Plot Data")
-   #x_column = st.selectbox("Select x axis", filtered_df)
-    #y_column = filtered_df.iloc[:,1]
 
+    d = st.date_input("Select the start date" , format = "DD/MM/YYYY")
+    d1 = pd.to_datetime(d, format = "%d/%m/%Y")
+    d2 = st.date_input("Select the end date",format = "DD/MM/YYYY")
+    d3 = pd.to_datetime(d2, format = "%d/%m/%Y")
+   
+    filtered_df2 = df[(df['Date'] > d) & (df['Date'] < d2)]
+
+    st.subheader("Summary of the selected category")
+    st.write(filtered_df2.head())
+    
     
     if st.button("Generate Plot"):
-        st.bar_chart(filtered_df, x=filtered_df.iloc(:,1), y=filtered_df.iloc(:,2))
-    st.write("Waiting on file")
+        st.bar_chart(filtered_df, x="Sub-Category", y="Charges $")
+        st.write("Waiting on file")
